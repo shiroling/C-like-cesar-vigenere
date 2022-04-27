8c4066fb708134cd23ee8f956dd105adabe928fc
shiroling <contact.shiroling@gmail.com>
2022-04-27 17:48:08 +0200

    le libérateur

---
+++ controleEntree.c
@@ -0,0 +1,62 @@
+#include "controleEntree.h"
+
+//------------------------------
+//---------VERIFICATION---------
+//------------------------------
+
+int isAccent(wchar_t wstrIn) {
+    wchar_t accent[53] = L"ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÌÍÎÏìíîïÙÚÛÜùúûüÿÑñÇç";
+
+    for (int i = 0; i < 53; ++i)
+    {
+        if (wstrIn == accent[i])
+            return i;
+    }
+    return -1;
+}
+
+bool verifierAlphanumérique(wchar_t *wstr) {
+    for (int i = 0; i < wcslen(wstr); ++i)
+    {
+        if (!iswalnum(wstr[i]) && isAccent(wstr[i]) == -1 && wstr[i] != L' ')
+        {
+            wprintf(L"Le char : '%c' n'est pas alphaNum\n", wstr[i]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    return true;
+}
+
+
+//------------------------------
+//----------TRAITEMRNT----------
+//------------------------------
+
+char *convertirAccents(wchar_t *wstrIn) {
+    // 2nd partie de la table de correspondance des accents, la premiére étant dans int isAccent(wchar_t wstrIn).
+    char sansAccent[53] = "AAAAAAaaaaaaOOOOOOooooooEEEEeeeeIIIIiiiiUUUUuuuuyNnCc";
+    int taille = wcslen(wstrIn), index;                                 //taille de la chaine et index pour la table de correspondace des accents.
+    char *messageNonAccent = (char*)calloc(sizeof(char), taille);       // Allocation dynamique de la chaine de charactére en entrée.
+    
+    for (int i=0;i<taille;i++)              // on parcourt la chaine d'entrée
+    {
+        index = isAccent(wstrIn[i]);        // on teste si il y a un accent à la position courante
+        if (index != -1)
+        {                                   // Si oui on le remplace 
+            messageNonAccent[i] = sansAccent[index];
+        }else                               // Si non on convertis la charactére pour le bon type
+        {
+            messageNonAccent[i] = wstrIn[i];
+        }
+
+    }
+    return messageNonAccent;
+}
+
+char *replaceSpaces(char* str) {
+    for (int i = 0; i < strlen(str); ++i)
+        if (str[i] == ' ')
+            str[i] = '`'; 
+
+    return str;
+}
---
+++ controleEntree.h
@@ -0,0 +1,39 @@
+#include <stdio.h>      // I/O
+#include <stdlib.h>     // exit failue
+
+#include <string.h>     // fonction sur les cahines de charactéres
+#include <stdbool.h>    // Type de donnée Boolean non natif en C
+
+// support du type de donné wchar_t permettant la gestion des accents
+#include <wchar.h>
+#include <wctype.h>
+
+//------------------------------
+//---------VERIFICATION---------
+//------------------------------
+/*  Fonction testant si un charactére large donné est accentué (fait partie de la table prédéfinie 'accent')
+    si il fait parti de la table, la fonction renvoie son indice dans la table
+    Sinon -1 (0 étant pris par l'indice de la premiére case de la table 'accents')                          */
+int isAccent(wchar_t wstrIn);
+
+/*  Fonction testant si une chaine charactére large donné est alphanumérique (comprenant le accents)
+    si Si oui retourne vrai 
+    Sinon provoque une sorte de programe comme demandé dans le sujet                                        */
+bool verifierAlphanumérique(wchar_t *wstr);
+
+
+
+//------------------------------
+//----------TRAITEMENT----------
+//------------------------------
+
+/*  Cette fonction de traitement permet de convertir la chaine de charactéres large en chaine de charactères multibyte
+    pour cela nous convertissons les accents à partir d'une table de correspondance
+    puis nous copions les charactéres classiques dans la nouvelle cahines                                   */
+char *convertirAccents(wchar_t *wstrIn);
+
+/*  Fonction remplacant les espaces dans une chaine de charactére simple en un charactére définit
+    Afin qu'il puisse étre encodé par les fonctions de chiffrement.
+    Le charactére choisit est ` car il est adjacent au 'a' dans la table ascii et qu'il permet une bonne
+    lecture du message une fois qu'il remplace les espaces                                                  */
+char *convertirEspaces(char* str);
